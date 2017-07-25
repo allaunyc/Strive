a09d32f8d60581938e17e22ea03ebf16b54f78a8
vanessacortes <vcortes@berkeley.edu>
2017-07-25 11:51:19 -0700

    add sendgrid

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
+app.use(bodyParser.json())
+
+var routes = require('./sendgrid');
+
 app.use(express.static(__dirname));
-app.listen(3000);
+app.use('/', routes);
+
+app.listen(process.env.PORT || '3000', function() {
+  console.log('The server is up!');
+})
---
+++ env.sh
@@ -0,0 +1,2 @@
+export SENDGRID_API_KEY="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";
+export TEMPLATE_ID="791fb4ee-ecf0-48ae-9bef-ec34170bff4e";
